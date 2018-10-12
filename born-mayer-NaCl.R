3b2a1de56963748e86cfb4b3d2600831d7a43878
Benjamin Obi Tayo <benjaminobi@gmail.com>
2018-10-12 16:34:25 -0500

    Update born-mayer-NaCl.R

--- born-mayer-NaCl.R
+++ born-mayer-NaCl.R
@@ -15,14 +15,14 @@ Born_Mayer<-function(x)(1.491+2692*exp(-1.837*x) - 27.2/x)
 # apply Born-Mayer function to x coordinate to generate potential energy for each x
 V<-sapply(x, Born_Mayer)
 
-# store coordinate and potential energy into a data frame
+# store coordinate and potential energy in a data frame
 data<-data.frame(x=x,V=V)
 
 # generate visualization of potential energy
 data%>%ggplot(aes(x,V))+
   geom_line(size=1.1)+
   xlab("x (a.u) ")+
-  ylab("V(x)")+
+  ylab("V (eV)")+
   ggtitle("Born-Mayer potential energy for NaCl")+
   theme(plot.title = element_text(color="black", size=12, hjust=0.5, face="bold"),
           axis.title.x = element_text(color="black", size=12, face="bold"),
